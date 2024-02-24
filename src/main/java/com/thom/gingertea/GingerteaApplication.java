24a5650b4a3c0947483f2994bcfff8282a550866
thom-droid <thom-droid@github.com>
2024-02-24 15:21:26 +0900

    initial commit

---
+++ src/main/java/com/thom/gingertea/GingerteaApplication.java
@@ -0,0 +1,13 @@
+package com.thom.gingertea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GingerteaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GingerteaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thom/gingertea/GingerteaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thom.gingertea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GingerteaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
